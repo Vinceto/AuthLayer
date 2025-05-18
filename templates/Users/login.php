d27deaa58ab9128fb504a7f985354273b96685d4
Ramon Bernabe Munoz Navarrete <rmkmegod@gmail.com>
2025-05-17 22:19:12 -0400

    ajustes a css de login

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,29 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Idiem</title>
+    <title>Idiem - Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <style>
-        
-
+       
     </style>
 </head>
 <body>
-    <div id="loginnuevo">
-        <h2>Iniciar sesión</h2>
-
+    <div class="login-container">
+        <h2>In<span class="idiem-i">i</span>c<span class="idiem-i">i</span>ar ses<span class="idiem-i">i</span>ón</h2>
         <?= $this->Form->create() ?>
-        <table>
-            <tr>
-                <td><?= $this->Form->control('email', ['label' => 'Usuario', 'required' => true]) ?></td>
-            </tr>
-            <tr>
-                <td><?= $this->Form->control('password', ['label' => 'Contraseña', 'required' => true]) ?></td>
-            </tr>
-            <tr>
-                <td><?= $this->Form->button('Entrar', ['class' => 'btn']) ?></td>
-            </tr>
-        </table>
+
+            <?= $this->Form->control('email', [
+                'label' => 'Usuario',
+                'required' => true,
+                'type' => 'email',
+                'placeholder' => 'correo@idiem.cl'
+            ]) ?>
+
+            <?= $this->Form->control('password', [
+                'label' => 'Contraseña',
+                'required' => true,
+                'placeholder' => '••••••••'
+            ]) ?>
+
+            <?= $this->Form->button('Entrar', ['class' => 'button-primary']) ?>
+
         <?= $this->Form->end() ?>
     </div>
 </body>
